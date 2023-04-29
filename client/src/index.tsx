02e3e13d0c27eb3ab0dbc42c863bdd0e897952d8
birehan <birehananteneh4@gmail.com>
2023-04-29 22:14:19 +0300

    module 8

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,13 +4,15 @@ import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 import { StoreContext, store } from "./app/stores/store";
 import "./app/layout/styles.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/routes/Routes";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <RouterProvider router={router} />
   </StoreContext.Provider>
 );
 
